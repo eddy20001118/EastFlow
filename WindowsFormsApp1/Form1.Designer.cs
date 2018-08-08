8571ea0706b713ff94b5d86585336a39cd11b94e
eddy20001118 <1416964247@qq.com>
2018-08-08 16:05:34 +0800

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Gen = new System.Windows.Forms.Button();
+            this.Res = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(5, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 0);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Gen
+            // 
+            this.Gen.BackColor = System.Drawing.SystemColors.Control;
+            this.Gen.FlatAppearance.BorderSize = 0;
+            this.Gen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Gen.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Gen.Location = new System.Drawing.Point(5, 555);
+            this.Gen.Name = "Gen";
+            this.Gen.Size = new System.Drawing.Size(55, 30);
+            this.Gen.TabIndex = 1;
+            this.Gen.Text = "生成";
+            this.Gen.UseVisualStyleBackColor = false;
+            // 
+            // Res
+            // 
+            this.Res.BackColor = System.Drawing.SystemColors.Control;
+            this.Res.FlatAppearance.BorderSize = 0;
+            this.Res.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Res.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Res.Location = new System.Drawing.Point(66, 555);
+            this.Res.Name = "Res";
+            this.Res.Size = new System.Drawing.Size(55, 30);
+            this.Res.TabIndex = 2;
+            this.Res.Text = "重置";
+            this.Res.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(500, 630);
+            this.Controls.Add(this.Res);
+            this.Controls.Add(this.Gen);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("幼圆", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.Text = "East Flow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Gen;
+        private System.Windows.Forms.Button Res;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,231 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EastFlow.Properties;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MaterialForm
+    {
+        public static int N = 0;
+        public static int[] colorTag = new int[64];
+        public static int HowManyzero;
+        string path3 = System.IO.Directory.GetCurrentDirectory() + @"\solver.exe";
+        FileStream fs;
+        FileStream res;
+        StreamWriter sw;
+        StreamReader sr;
+        Process p;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitColorTag();
+            AddPictureBox();
+            Generate();
+        }
+
+        private void InitColorTag()
+        {
+            for (int i = 0; i < 64; i++)
+            {
+                colorTag[i] = 0;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue800, Primary.Blue800, Accent.LightBlue200, TextShade.WHITE);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.FormClosing += Form1_FormClosing;
+    }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            if (sr != null)
+            {
+                sr.Close();
+                File.Delete("testdata.out");
+            }
+            sw.Close();
+            fs.Close();
+            File.Delete("solver.exe");
+            File.Delete("testdata.in");
+        }
+
+        Button[] Btn = new Button[64];
+        private void AddPictureBox()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    int N = i * 8 + j;
+                    Btn[N] = new Button()
+                    {
+                        Height = 55,//每个格子高度 宽度均为70
+                        Width = 55,//
+                        Top =i * 60,//起始坐标
+                        Left =j * 60
+                    };
+                    Btn[N].BackColor = Color.FromArgb(255,245,245,245);
+                    Btn[N].FlatStyle = FlatStyle.Flat;
+                    Btn[N].FlatAppearance.BorderSize = 0;
+                    this.panel1.Controls.Add(Btn[N]);
+                    Btn[N].Click += delegate (object sender, EventArgs e)
+                    {
+                        Button btn = (Button)sender;
+                        int M = N;
+                        btn.BackgroundImage = Image.FromFile(@"C:\Users\yuhao\source\repos\WindowsFormsApp1\WindowsFormsApp1\Res\castle.png");
+                        btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+                        switch (colorTag[M])
+                        {
+                            case 0: btn.BackColor = Color.FromArgb(255,205,92,92); colorTag[M] = 1; break; //red
+                            case 1: btn.BackColor = Color.FromArgb(255,46,139,87); colorTag[M] = 2; break; //green
+                            case 2: btn.BackColor = Color.FromArgb(255,238,221,130); colorTag[M] = 3; break;  //yellow
+                            case 3: btn.BackColor = Color.FromArgb(255,65,105,225); colorTag[M] = 4; break;
+                            case 4:
+                                btn.BackgroundImage = null; 
+                                btn.BackColor = Color.FromArgb(255, 245, 245, 245);
+                                btn.FlatStyle = FlatStyle.Flat;
+                                btn.FlatAppearance.BorderSize = 0;
+                                colorTag[M] = 0;
+                                break;
+                        }
+                    };
+                }
+            }
+        }
+
+        private void Generate()
+        {
+            fs = new FileStream("testdata.in", FileMode.OpenOrCreate);
+            sw = new StreamWriter(fs);
+            Button generate = this.Gen;
+            Button Reset = this.Res;
+            generate.Click += delegate (object sender, EventArgs e)
+            {
+                fs.SetLength(0);
+                HowManyzero = 0;
+                for(int q = 0; q < 64; q++)
+                {
+                    if(colorTag[q] == 0)
+                    {
+                        HowManyzero++;
+                    }
+                }
+                if (HowManyzero != 48)
+                {
+                    MessageBox.Show("输入错误");
+                }
+                else
+                {
+                    for (int n = 0; n < 8; n++)
+                    {
+                        for (int m = 0; m < 8; m++)
+                        {
+                            sw.Write(colorTag[n * 8 + m]);
+                            sw.Write(" ");
+                        }
+                        sw.WriteLine();
+                    }
+                    sw.Flush();
+                    res = new FileStream(path3, FileMode.Create);
+                    res.Write(Resources.solver, 0, Resources.solver.Length);
+                    res.Close();
+                    p = new Process();
+                    p.StartInfo.FileName = "solver.exe";
+                    p.StartInfo.CreateNoWindow = true;
+                    p.Start();
+                    p.WaitForExit(15*1000);
+                    if(p.HasExited == true)
+                    {
+                        Thread.Sleep(100);
+                        ReadColor();
+                        Stop_Process();
+                    }
+                    else
+                    {
+                        MessageBox.Show("无解");
+                        p.CloseMainWindow();
+                        Stop_Process();
+                    }
+                }
+                
+            };
+            Reset.Click += delegate (object sender, EventArgs e)
+            {
+                fs.SetLength(0);
+                InitColorTag();
+                for(int N = 0; N < 64; N++)
+                {
+                    Btn[N].BackgroundImage = null;
+                    Btn[N].BackColor = Color.FromArgb(255, 245, 245, 245);
+                    Btn[N].FlatStyle = FlatStyle.Flat;
+                    Btn[N].FlatAppearance.BorderSize = 0;
+                }
+            };
+        }
+        private void ReadColor()
+        {
+            sr = new StreamReader("testdata.out");
+            String str_read = sr.ReadToEnd();
+            int[] ReadColorTag = new int[64];
+            sr.Close();
+            str_read = str_read.Replace(" ", "");
+            str_read = str_read.Replace("\r\n", "");
+            if (str_read.Length != 64)
+            {
+                MessageBox.Show("无解");
+            }
+            else
+            {
+                for (int m = 0; m < 64; m++)
+                {
+                    int n = m;
+                    int swit = Convert.ToInt32(str_read[n].ToString());
+                    switch (swit)
+                    {
+                        default: Btn[n].BackColor = Color.FromArgb(255, 245, 245, 245); break; //default color
+                        case 1: Btn[n].BackColor = Color.FromArgb(255, 205, 92, 92); break; //red
+                        case 2: Btn[n].BackColor = Color.FromArgb(255, 46, 139, 87); break; //green
+                        case 3: Btn[n].BackColor = Color.FromArgb(255, 238, 221, 130); break;  //yellow
+                        case 4: Btn[n].BackColor = Color.FromArgb(255, 65, 105, 225); break;
+                    }
+
+                }
+            }
+            
+        }
+        private void Stop_Process()
+        {
+            Process[] ps = Process.GetProcesses();//获取计算机上所有进程
+            foreach (Process p in ps)
+            {
+                if (p.ProcessName == "solver")//判断进程名称
+                {
+                    p.Kill();//停止进程
+                }
+            }
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+    }
+}
